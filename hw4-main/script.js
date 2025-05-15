13232fa5bdb40261f5a973c7300615432c44c9a4
jordangatewood28 <165770705+jordangatewood28@users.noreply.github.com>
2025-05-15 05:01:12 -0700

    Update script.js

--- hw4-main/script.js
+++ hw4-main/script.js
@@ -19,23 +19,5 @@ async function register() {
     const username = document.getElementById().value;
     const password = document.getElementById().value;
 
-    try {
-        const response = await fetch(`${API}/register`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({username, password})
-        });
-        if (response.status===201){
-            const data = await response.json();
-            alert("Registration successul. Please lgon.")
-            clearInput();
-        } else {
-            const error = await response.text();
-            alert(`Registration failed: ${error}`)
-        }
-    }
-    catch (error) {
-    alert(`Error: ${error.message}`);}
+    
 }
