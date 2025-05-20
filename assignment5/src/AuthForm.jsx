ee181699b506329185fec7ea2d0d9e1af2d327d2
jordangatewood28 <165770705+jordangatewood28@users.noreply.github.com>
2025-05-20 14:30:58 -0700

    Add files via upload

---
+++ assignment5/src/AuthForm.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+function AuthForm() {
+
+    return(
+    <div>
+        <h2>Login</h2>
+        <form>
+            <div>
+                <input type='text' placeholder="enter your email" />
+            </div>
+        </form>
+    </div>)
+}
+
+export default AuthForm
